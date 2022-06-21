58564f67a035486ee75a7aac834c541f824b051a
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-06-21 18:04:54 +0700

    fix error with unusual imports

--- src/components/card-container/card-container.jsx
+++ src/components/card-container/card-container.jsx
@@ -1,9 +1,5 @@
 import {CardComponent} from "../card-component/card-component";
 import {getPostfix} from "../../helpers/helpers";
-import ReactCursorPosition from "react-cursor-position";
-import {INTERACTIONS} from "react-cursor-position/dist/constants";
-
-
 
 const CardContainer = ({ingredient, description, pieces, presents, weight, inStock}) => {
     const piecesWord = `${pieces} ${getPostfix(pieces, 'pieces')}`
@@ -12,19 +8,15 @@ const CardContainer = ({ingredient, description, pieces, presents, weight, inSto
     const weightDelimeter = weight >= 1 ? weight : `${0},${weight * 10}`
 
     return (
-        <ReactCursorPosition
-            activationInteractionMouse={INTERACTIONS.HOVER}
-        >
-            <CardComponent
-                ingredient={ingredient}
-                description={description}
-                pieces={`${piecesWord}`}
-                presents={`${presentsWord}`}
-                addedWords={addedWords}
-                weight={weightDelimeter}
-                inStock={inStock}
-            />
-        </ReactCursorPosition>
+        <CardComponent
+            ingredient={ingredient}
+            description={description}
+            pieces={`${piecesWord}`}
+            presents={`${presentsWord}`}
+            addedWords={addedWords}
+            weight={weightDelimeter}
+            inStock={inStock}
+        />
     );
 };
 
